7ff3acdaacbb8ad254d57226d591b0981900f974
Eon <eoneon123@gmail.com>
2016-05-31 14:04:22 -0700

    generated controller resources, edited routes.rb, updated views and #index

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
+  resources :posts
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
+  get 'about' => 'welcome#about'
+  
   root 'welcome#index'
 
 end
