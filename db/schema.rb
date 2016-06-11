1d3032241bd42e62c8b6587a75dc7adbd61daa0f
Eon <eoneon123@gmail.com>
2016-06-10 17:21:25 -0700

    generated model user, validations, and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607000830) do
+ActiveRecord::Schema.define(version: 20160610224444) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(version: 20160607000830) do
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
