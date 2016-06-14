44cb179b899034bc898fc26f50de76fff436996b
Eon <eoneon123@gmail.com>
2016-06-13 20:17:05 -0700

    associate users and posts, added require_sign_in, refactored topics/posts controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 require 'random_data'
 
+5.times do
+  User.create!(
+    name:     RandomData.random_name,
+    email:    RandomData.random_email,
+    password: RandomData.random_sentence
+    )
+end
+users = User.all
+
 15.times do
    Topic.create!(
      name:         RandomData.random_sentence,
@@ -10,6 +19,7 @@ require 'random_data'
 
 50.times do
   Post.create!(
+    user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -20,6 +30,7 @@ posts = Post.all
 
 50.times do
   SponsoredPost.create!(
+    # user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -35,7 +46,14 @@ sponsored_posts = SponsoredPost.all
   )
 end
 
+# user = User.first
+# user.update_attributes!(
+#   email: 'eoneon123@gmail.com',
+#   password: 'helloworld'
+# )
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{SponsoredPost.count} sponsored_posts created"
