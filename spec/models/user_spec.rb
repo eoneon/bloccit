fd2605987264343c20622d9630d85152855ff8f3
Eon <eoneon123@gmail.com>
2016-06-22 18:53:11 -0700

    updated

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,4 +33,11 @@ RSpec.describe User, type: :model do
       expect(user_with_invalid_email).to_not be_valid
     end
   end
+
+  # describe "should be a valid user due to capitalization" do
+  #   it "has a capitalized name" do
+  #     let(:name) { "eon mcLeary" }
+  #     expect(user.name).to eq "Eon McLeary"
+  #   end
+  # end
 end
