19173301637515e8f58286427d2b4e3ab473ac42
Eon <eoneon123@gmail.com>
2016-06-23 18:45:17 -0700

    assignment-37

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
+
+  post 'users/confirm' => 'users#confirm'
+
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
