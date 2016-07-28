bec6e56cc90c4f8e4fed717ea138b5671ceea9fe
Eon <eoneon123@gmail.com>
2016-07-27 17:53:05 -0700

    assignment-44

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,14 +2,24 @@ class FavoriteMailer < ApplicationMailer
   default from: "eoneon123@gmail.com"
     def new_comment(user, post, comment)
 
-    headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
-    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
-    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+      headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+      headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+      headers["References"] = "<post/#{post.id}@your-app-name.example>"
 
-    @user = user
+      @user = user
+      @post = post
+      @comment = comment
+
+      mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<posts/#{post.id}@your-app-name.example>"
+    headers["References"] = "<posts/#{post.id}@your-app-name.example>"
+
     @post = post
-    @comment = comment
 
-    mail(to: user.email, subject: "New comment on #{post.title}")
+    mail(to: post.user.email, subject: "You've just favorited the post #{post.title} and will recieve updates when it's commented on.")
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
 
@@ -14,6 +14,8 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+  after_create :create_favorite
 
   def up_votes
     votes.where(value: 1).count
@@ -32,4 +34,10 @@ class Post < ActiveRecord::Base
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
   end
+
+  def create_favorite
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
+  end
+
 end
