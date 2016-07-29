3a23b41bdb84d839fe07a6b234b64e160a50229b
Eon <eoneon123@gmail.com>
2016-07-29 09:36:06 -0700

    checkpoint-42 update

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,8 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :sponsored_posts
-  
+  has_many :comments 
+
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
 end
