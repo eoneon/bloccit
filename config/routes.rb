001102bb0581f18df6d6f42e493f93d89b23c528
Eon <eoneon123@gmail.com>
2016-07-31 15:34:00 -0700

    assignment-42-revisit

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,13 @@ Rails.application.routes.draw do
 
   resources :topics do
     resources :posts, except: [:index]
+    resources :comments, only: [:create, :destroy]
     resources :sponsored_posts, except: [:index]
   end
 
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
   end
-
-  # resources :topics, only: [] do
-  #   resources :comments, only: [:create, :destroy]
-  # end
 
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
